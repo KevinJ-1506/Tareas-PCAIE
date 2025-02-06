291ea82b97e149f3529405d3825d5088c7dc386f
KevinJ-1506 <3214435510502@ingenieria.usac.edu.gt>
2025-02-05 18:26:51 -0600

    Add files via upload
    Tarea 2 . En esta tarea se tenia como objetivo el uso de la base de datos PostgreSQL mediante el uso de GNU Octave

---
+++ Tarea2/basse de datos.sql
@@ -0,0 +1,10 @@
+--Create table redes(
+		--Nombre varchar(20),
+		--carnet int not null
+--)
+--select*from redes;
+--insert into redes values('Kevin Hernandez',201904037);
+--select*from redes;
+insert into redes values('Carlos Medina',202100144);
+insert into redes values('Cindy Gatica',201700234);
+select*from redes;
