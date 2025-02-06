49262d0afd16cfd34223c8bfeb3415bc25e762af
KevinJ-1506 <3214435510502@ingenieria.usac.edu.gt>
2025-02-05 18:21:32 -0600

    Add files via upload
    tarea2 de la clase. en esta se tenia como objetivo el manejo de PostgreSQL por medio de GNU Octave

---
+++ Tarea3/base de datos tarea2.sql
@@ -0,0 +1,8 @@
+--Create table redes (
+	--Nombre varchar(20),
+	--Carnet int not null
+--)
+--select*from redes;
+--insert into redes values('Kevin Hernandez',201904037);
+insert into redes values('Carlos Medina',202100144);
+select*from redes;
