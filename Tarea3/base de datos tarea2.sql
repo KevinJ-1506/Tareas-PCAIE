fcd4d4b36ca4fdd40f6984c27b16eee5527c5119
KevinJ-1506 <3214435510502@ingenieria.usac.edu.gt>
2025-02-05 20:26:10 -0600

    Delete Tarea3/base de datos tarea2.sql

--- Tarea3/base de datos tarea2.sql
+++
@@ -1,8 +0,0 @@
---Create table redes (
-	--Nombre varchar(20),
-	--Carnet int not null
---)
---select*from redes;
---insert into redes values('Kevin Hernandez',201904037);
-insert into redes values('Carlos Medina',202100144);
-select*from redes;
